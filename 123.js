0b778907a2445dd5416af18fd53d7aadf7b0fff2
m1crosofe <masnyuk2000@mail.ru>
2025-04-29 21:31:35 +0500

    registration backend

---
+++ 123.js
@@ -0,0 +1,5 @@
+const users = [
+    {id: 1, login: 'admin@ex.ru', password: 'admin'},
+    {id: 2, login: 'user@ex.ru', password: 'user'}
+]
+
--- index.js
+++ index.js
@@ -7,7 +7,11 @@ const port = 3000;
 const options = {
     root: "routes"
 }
-
+app.use(express.urlencoded({ extended: true }));
+const users = [
+    {id: 1, login: 'admin@ex.ru', password: 'admin', date: '01.01.2025'},
+    {id: 2, login: 'user@ex.ru', password: 'user', date: '02.02.2025'}
+]
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
@@ -22,8 +26,33 @@ app.get('/registration', (req, res) => {
     res.sendFile('registration.html', options)
 })
 
+app.post('/registration', (req, res) => {
+    try {
+
+        const { email, password } = req.body;
+        const id = users.length + 1;
+        var today = new Date();
+        var day = today.getDate().toString()
+        var month = today.getMonth()+1
+        var year = today.getFullYear().toString()
+        var date = day+'.'+month+'.'+year
+        users.push({ id: id, login: email, password: password, date: date })
+        return res.redirect('/profile')
+        
+    }
+    catch (e) {
+        return res.statusCode(400)
+    }
+    
+  
+});
+
 app.get('/service', (req, res) => {
     res.sendFile('service.html', options)
+})
+app.get('/profile', (req, res) => {
+    res.sendFile('profile.html', options)
+    console.log(users);
 })
 app.listen(port, () => {
     console.log(`Сервер запущен, http://localhost:${port}`);
